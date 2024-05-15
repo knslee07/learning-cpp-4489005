4750e57a3ac6ebc0ebc41d62f880a2b0b8d5b39a
Kwan Lee <knslee07@gmail.com>
2024-05-15 04:47:19 +0000

    did first part of ch2

--- src/Ch02/02_03b/CodeDemo.cpp
+++ src/Ch02/02_03b/CodeDemo.cpp
@@ -4,9 +4,22 @@
 
 #include <iostream>
 
+int a, b =5; //a and b are global.
+
 int main(){
-    std::cout << "Hi There!" << std::endl;
-    
+    bool my_flag;
+    a=7;
+    my_flag=false;
+
+    std::cout << "a? " << a << std::endl;
+    std::cout << "b? " << b << std::endl;
+    std::cout << "my flag? " << my_flag << std::endl;
+    std::cout << "a+b? " << a+b << std::endl;
+    unsigned int positive;
+    positive = b-a;
+    std::cout << "positive? " << positive << std::endl;
+    // This is because 5 - 7 is -2, which is converted to an unsigned int, resulting in a large positive number.
+
     std::cout << std::endl << std::endl;
     return (0);
 }
