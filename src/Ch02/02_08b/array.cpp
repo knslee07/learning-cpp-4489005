579050f454d90787bb772d45b4c8fe06ed487db6
Kwan Lee <knslee07@gmail.com>
2024-05-16 06:27:18 +0000

    implicit conversion explanation added in array.cpp

--- src/Ch02/02_08b/array.cpp
+++ src/Ch02/02_08b/array.cpp
@@ -10,6 +10,10 @@ int main(){
     int age[AGE];
     float temp[]= {31.5, 32.7, 38.9};
 
+/*Implicit conversion, also known as type coercion, is the automatic conversion of a value from one data type to another by the compiler. This happens without any explicit instruction from the programmer. Implicit conversion typically occurs in operations that involve mixed data types, where the compiler converts one or more operands to a common type to perform the operation.
+
+In the case of the array initialization float temp[] = {31.5, 32.7, 38.9};, implicit conversion is happening because the values provided in the initializer list are of type double by default (as all floating-point literals in C and C++ are of type double unless specified otherwise), but they are being assigned to an array of type float.*/
+
     age[0]=25;
     age[1]=20;
     age[2]=19;
